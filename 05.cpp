846aa7f5c070233e22676592684bb1c0c587b13b
bielydev <gabrielprogramando220@gmail.com>
2025-08-26 03:25:17 -0300

    Aula 5 DEFINE

---
+++ 05.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+#define NAME "Aula"
+
+#define PI 3.1415
+
+#define get_defines cout << "NAME: " << NAME << " | PI: " << PI << endl;
+
+
+int main(){
+    int vida = 100, balas = 25, vidas = 3;
+
+    get_defines;
+
+    return 0;
+}
